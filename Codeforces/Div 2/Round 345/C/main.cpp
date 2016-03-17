1c32a21c1a76752e8791d7ff9d597c17bb69a7a1
Chun-Hung Tseng <henrybear327@gmail.com>
2016-03-17 22:29:44 +1100

    Add solution 2 to C

--- Codeforces/Div 2/Round 345/C/main.cpp
+++ Codeforces/Div 2/Round 345/C/main.cpp
@@ -15,57 +15,18 @@ int main()
 {
     int n;
     scanf("%d", &n);
-
-    pair<int, int> inp[200100];
-    for(int i = 0; i < n; i++) {
-	int x, y;
-	scanf("%d %d", &x, &y);
-
-	inp[i].first = x;
-	inp[i].second = y;
-    }
-
-    sort(inp, inp + n);
-
-    map<int, int> xcnt, ycnt;
-    for(int i = 0; i < n; i++) {
-	xcnt[inp[i].first]++;
-	ycnt[inp[i].second]++;
-    }
-
+    
+    map<int, ll> X, Y;
+    map< pair<int, int>, ll > Z;
     ll ans = 0;
-    // x==x
-    for(auto i : xcnt) {
-	ll tmp = i.second;
-	if(tmp > 1)
-	    ans += (tmp * (tmp - 1) / 2);
+    while(n--) {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	ans += X[a] + Y[b] - Z[make_pair(a, b)];
+	X[a]++;
+	Y[b]++;
+	Z[make_pair(a, b)]++;
     }
-
-    // y==y
-    for(auto i : ycnt) {
-	ll tmp = i.second;
-	if(tmp > 1)
-	    ans += (tmp * (tmp - 1) / 2);
-    }
-
-    // x==x && y==y
-    pair<int, int> prev = inp[0];
-    int tmp_cnt = 1;
-    for(int i = 1; i <= n; i++) {
-	if(i != n && inp[i] == prev) {
-	    tmp_cnt++;
-	} else {
-	    if(tmp_cnt > 1)
-		// ans -= tmp_cnt * (tmp_cnt - 1) / 2;
-        ans -= tmp_cnt * (tmp_cnt - 1LL) / 2LL; // this way can avoid problems when forget to use ll for tmp_cnt
-
-	    tmp_cnt = 1;
-	    if(i == n)
-		break;
-	    prev = inp[i];
-	}
-    }
-
     printf("%lld\n", ans);
 
     return 0;
