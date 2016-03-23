90f8dc8fda4a74818a105f4fdd5f15dc5785f95e
Chun-Hung Tseng <henrybear327@gmail.com>
2016-03-23 23:43:33 +1100

    Astyled two files

--- Codeforces/Div 2/IndiaHacks 2016 - Online Edition/B/main.cpp
+++ Codeforces/Div 2/IndiaHacks 2016 - Online Edition/B/main.cpp
@@ -12,10 +12,10 @@ int main()
 
     pair< string, vector<string> > op[6];
     for(int i = 0; i < k; i++) {
-	string s1, s2;
-	cin >> s1 >> s2;
+        string s1, s2;
+        cin >> s1 >> s2;
 
-	op[s2[0] - 'a'].second.push_back(s1);
+        op[s2[0] - 'a'].second.push_back(s1);
     }
 
     string str = "a";
@@ -24,20 +24,20 @@ int main()
     set<string> ans;
 
     while(!q.empty()) {
-	string cur = q.front();
-	q.pop();
-	if((int)cur.length() == n) {
-	    ans.insert(cur);
-	    continue;
-	}
+        string cur = q.front();
+        q.pop();
+        if((int)cur.length() == n) {
+            ans.insert(cur);
+            continue;
+        }
 
-	for(int j = 0; j < (int)op[cur[0] - 'a'].second.size(); j++) {
-	    string tmp = cur;
-	    tmp.insert(0, op[cur[0] - 'a'].second[j]);
-	    tmp.erase(0 + 2, 1);
+        for(int j = 0; j < (int)op[cur[0] - 'a'].second.size(); j++) {
+            string tmp = cur;
+            tmp.insert(0, op[cur[0] - 'a'].second[j]);
+            tmp.erase(0 + 2, 1);
 
-	    q.push(tmp);
-	}
+            q.push(tmp);
+        }
     }
     printf("%d\n", (int)ans.size());
 
