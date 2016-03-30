8a8bea250aa7abb08755c1cfd280369920b235cf
Chun-Hung Tseng <henrybear327@gmail.com>
2016-03-31 10:57:40 +1100

    Update java folder template

--- Codeforces/templates/contest/C/C.java
+++ Codeforces/templates/contest/C/C.java
@@ -8,24 +8,8 @@ public class C
         MyScanner sc = new MyScanner();
         out = new PrintWriter(new BufferedOutputStream(System.out));
 
-        // Start writing your solution here.
-
-        /*
-        int n      = sc.nextInt();        // read input as integer
-        long k     = sc.nextLong();       // read input as long
-        double d   = sc.nextDouble();     // read input as double
-        String str = sc.next();           // read input as String
-        String s   = sc.nextLine();       // read whole line as String
-
-    	while(sc.hasNext()) {
-    		...
-    	}
-
-        int result = 3*n;
-        out.println(result);                    // print via PrintWriter
-        */
-
-        // Stop writing your solution here.
+        
+        
         out.close();
     }
 
