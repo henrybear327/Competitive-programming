c6309ad9fd52fc876edbeed0a37b62caaaeaf3f8
Henry Tseng <henrybear327@gmail.com>
2016-04-26 23:32:26 +1000

    Use astyle to format code

--- etutor/PTC/April 2016/C/main.cpp
+++ etutor/PTC/April 2016/C/main.cpp
@@ -2,19 +2,19 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-    using namespace std;
+using namespace std;
 
-    typedef long long ll;
-    vector<ll> num;
-    vector<string> gen;
+typedef long long ll;
+vector<ll> num;
+vector<string> gen;
 
-    ll rToNumber(string x)
-    {
-	// 12345
-	ll res = 0;
-	for(int i = (int)x.length() - 1; i >= 0; i--) {
-	res *= 10;
-	res += (x[i] - '0');
+ll rToNumber(string x)
+{
+    // 12345
+    ll res = 0;
+    for(int i = (int)x.length() - 1; i >= 0; i--) {
+        res *= 10;
+        res += (x[i] - '0');
     }
     //printf("%s %lld\n", x.c_str(), res);
     return res;
@@ -24,8 +24,8 @@ ll toNumber(string x)
     // 12345
     ll res = 0;
     for(int i = 0; i < (int)x.length(); i++) {
-	res *= 10;
-	res += (x[i] - '0');
+        res *= 10;
+        res += (x[i] - '0');
     }
     //printf("%s %lld\n", x.c_str(), res);
     return res;
@@ -36,7 +36,7 @@ void powgen()
 {
     pow10[0] = 1;
     for(int i = 1; i <= 18; i++) {
-	pow10[i] = pow10[i - 1] * 10;
+        pow10[i] = pow10[i - 1] * 10;
     }
 }
 
@@ -47,55 +47,55 @@ void init()
     int prev = 0;
     int pos[20] = {0};
     for(int i = 1; i <= 8; i++) {
-	int len = gen.size();
-	pos[i] = prev; 
-	for(int j = 0; prev + j < len; j++) {
-	    gen.push_back(gen[prev + j] + "4");
-	    gen.push_back(gen[prev + j] + "7");
-	}
-	pos[i + 1] = len; 
-	prev = len;
+        int len = gen.size();
+        pos[i] = prev;
+        for(int j = 0; prev + j < len; j++) {
+            gen.push_back(gen[prev + j] + "4");
+            gen.push_back(gen[prev + j] + "7");
+        }
+        pos[i + 1] = len;
+        prev = len;
     }
     pos[10] = 1022;
-   /* 
-    for(int i = 0; i < gen.size(); i++)
-	printf("%d %s %lld %lld\n", i, gen[i].c_str(), toNumber(gen[i]), rToNumber(gen[i]));
-    for(int i = 0; i <=10; i++)
-	printf("%d\n", pos[i]);
-    */
+    /*
+     for(int i = 0; i < gen.size(); i++)
+    printf("%d %s %lld %lld\n", i, gen[i].c_str(), toNumber(gen[i]), rToNumber(gen[i]));
+     for(int i = 0; i <=10; i++)
+    printf("%d\n", pos[i]);
+     */
 
     // odd
     num.push_back(4);
     num.push_back(7);
 
     for(int i = 1; i <= 8; i++) {
-	for(int j = pos[i]; j < pos[i + 1]; j++) {
-	    num.push_back(toNumber(gen[j]) * pow10[i + 1] + 4 * pow10[i] + rToNumber(gen[j]));
-	    num.push_back(toNumber(gen[j]) * pow10[i + 1] + 7 * pow10[i] + rToNumber(gen[j]));
-	}
+        for(int j = pos[i]; j < pos[i + 1]; j++) {
+            num.push_back(toNumber(gen[j]) * pow10[i + 1] + 4 * pow10[i] + rToNumber(gen[j]));
+            num.push_back(toNumber(gen[j]) * pow10[i + 1] + 7 * pow10[i] + rToNumber(gen[j]));
+        }
     }
-    
+
     // even
     for(int i = 1; i <= 9; i++) {
-	for(int j = pos[i]; j < pos[i + 1]; j++) {
-	    num.push_back(toNumber(gen[j]) * pow10[i] + rToNumber(gen[j]));
-	}
+        for(int j = pos[i]; j < pos[i + 1]; j++) {
+            num.push_back(toNumber(gen[j]) * pow10[i] + rToNumber(gen[j]));
+        }
     }
 }
 
-int main() 
+int main()
 {
     powgen();
     init();
     sort(num.begin(), num.end());
     int ncase;
     scanf("%d", &ncase);
-    
+
     while(ncase--) {
-	ll l, r;
-	scanf("%lld %lld", &l, &r);
-	
-	printf("%d\n", (int)(upper_bound(num.begin(), num.end(), r) - lower_bound(num.begin(), num.end(), l)));
+        ll l, r;
+        scanf("%lld %lld", &l, &r);
+
+        printf("%d\n", (int)(upper_bound(num.begin(), num.end(), r) - lower_bound(num.begin(), num.end(), l)));
     }
 
     return 0;
--- etutor/PTC/April 2016/C/main_test.cpp
+++ etutor/PTC/April 2016/C/main_test.cpp
@@ -14,8 +14,8 @@ ll rToNumber(string x)
     // 12345
     ll res = 0;
     for(int i = (int)x.length() - 1; i >= 0; i--) {
-	res *= 10;
-	res += (x[i] - '0');
+        res *= 10;
+        res += (x[i] - '0');
     }
     //printf("%s %lld\n", x.c_str(), res);
     return res;
@@ -25,8 +25,8 @@ ll toNumber(string x)
     // 12345
     ll res = 0;
     for(int i = 0; i < (int)x.length(); i++) {
-	res *= 10;
-	res += (x[i] - '0');
+        res *= 10;
+        res += (x[i] - '0');
     }
     //printf("%s %lld\n", x.c_str(), res);
     return res;
@@ -37,7 +37,7 @@ void powgen()
 {
     pow10[0] = 1;
     for(int i = 1; i <= 18; i++) {
-	pow10[i] = pow10[i - 1] * 10;
+        pow10[i] = pow10[i - 1] * 10;
     }
 }
 
@@ -48,44 +48,44 @@ void init()
     int prev = 0;
     int pos[20] = {0};
     for(int i = 1; i <= 8; i++) {
-	int len = gen.size();
-	pos[i] = prev; 
-	for(int j = 0; prev + j < len; j++) {
-	    gen.push_back(gen[prev + j] + "4");
-	    gen.push_back(gen[prev + j] + "7");
-	}
-	pos[i + 1] = len; 
-	prev = len;
+        int len = gen.size();
+        pos[i] = prev;
+        for(int j = 0; prev + j < len; j++) {
+            gen.push_back(gen[prev + j] + "4");
+            gen.push_back(gen[prev + j] + "7");
+        }
+        pos[i + 1] = len;
+        prev = len;
     }
     pos[10] = 1022;
-   /* 
-    for(int i = 0; i < gen.size(); i++)
-	printf("%d %s %lld %lld\n", i, gen[i].c_str(), toNumber(gen[i]), rToNumber(gen[i]));
-    for(int i = 0; i <=10; i++)
-	printf("%d\n", pos[i]);
-    */
+    /*
+     for(int i = 0; i < gen.size(); i++)
+    printf("%d %s %lld %lld\n", i, gen[i].c_str(), toNumber(gen[i]), rToNumber(gen[i]));
+     for(int i = 0; i <=10; i++)
+    printf("%d\n", pos[i]);
+     */
 
     // odd
     num.push_back(4);
     num.push_back(7);
 
     for(int i = 1; i <= 8; i++) {
-	for(int j = pos[i]; j < pos[i + 1]; j++) {
-	    num.push_back(toNumber(gen[j]) * pow10[i + 1] + 4 * pow10[i] + rToNumber(gen[j]));
-	    num.push_back(toNumber(gen[j]) * pow10[i + 1] + 7 * pow10[i] + rToNumber(gen[j]));
-	}
+        for(int j = pos[i]; j < pos[i + 1]; j++) {
+            num.push_back(toNumber(gen[j]) * pow10[i + 1] + 4 * pow10[i] + rToNumber(gen[j]));
+            num.push_back(toNumber(gen[j]) * pow10[i + 1] + 7 * pow10[i] + rToNumber(gen[j]));
+        }
     }
-    
+
     // even
     for(int i = 1; i <= 9; i++) {
-	for(int j = pos[i]; j < pos[i + 1]; j++) {
-	    num.push_back(toNumber(gen[j]) * pow10[i] + rToNumber(gen[j]));
-	}
+        for(int j = pos[i]; j < pos[i + 1]; j++) {
+            num.push_back(toNumber(gen[j]) * pow10[i] + rToNumber(gen[j]));
+        }
     }
 }
 
 /*
-int lb(int l, int r, ll key) 
+int lb(int l, int r, ll key)
 {
     int notFound = r;
     while(r - l > 1) {
@@ -102,7 +102,7 @@ int lb(int l, int r, ll key)
 */
 
 /*
-int ub(int l, int r, ll key) 
+int ub(int l, int r, ll key)
 {
     int notFound = r;
     while(r - l > 1) {
@@ -118,7 +118,7 @@ int ub(int l, int r, ll key)
 */
 
 /*
-int lb(int l, int r, ll key) 
+int lb(int l, int r, ll key)
 {
     r--;
     while(l < r) {
@@ -146,29 +146,29 @@ int ub(int l, int r, ll key)
 }
 */
 
-int lb(int l, int r, ll key) 
+int lb(int l, int r, ll key)
 {
     int cand = l;
     int s = r / 2;
     while(s != 0) { // [0, n)
-	if(cand + s < r && num[cand + s] < key) 
-	    cand += s;
-	else
-	    s /= 2;
+        if(cand + s < r && num[cand + s] < key)
+            cand += s;
+        else
+            s /= 2;
     }
 
     return cand;
-}   
+}
 
-int ub(int l, int r, ll key) 
+int ub(int l, int r, ll key)
 {
     int cand = l;
     int s = r / 2;
     while(s != 0) { // [0, n)
-	if(cand + s < r && num[cand + s] <= key) 
-	    cand += s;
-	else
-	    s /= 2;
+        if(cand + s < r && num[cand + s] <= key)
+            cand += s;
+        else
+            s /= 2;
     }
 
     return cand;
@@ -176,7 +176,7 @@ int ub(int l, int r, ll key)
 
 int rrrr = 0;
 
-int main() 
+int main()
 {
     srand(time(NULL));
 
@@ -185,35 +185,35 @@ int main()
     sort(num.begin(), num.end());
     int ncase = 10000;
     if(rrrr == 0)
-	scanf("%d", &ncase);
-    
+        scanf("%d", &ncase);
+
     while(ncase--) {
-	ll l, r;
-	if(rrrr == 0)
-	    scanf("%lld %lld", &l, &r);
-	else {
-	    l = rand() % 10;
-	    r = l + rand() % 10;
-	    //printf("%lld %lld\n", l, r);
-	}
-	
-	int lbb = lb(0, num.size(), l);
-	int ubb = ub(0, num.size(), r);
-	//printf("%d %d\n", ub(0, num.size(), r), (int)(upper_bound(num.begin(), num.end(), r) - num.begin()));
-	
-	if(rrrr == 0) {
-	    /*
-	    printf("lb diff %d %d\n", lbb,  (int)(lower_bound(num.begin(), num.end(), l) -  num.begin()));
-	    printf("ub diff %d %d\n", ubb, (int)(upper_bound(num.begin(), num.end(), r) -  num.begin()));
-	    printf("%d\n", ubb - lbb);
-	    */
-	    printf("%d\n", (int)(upper_bound(num.begin(), num.end(), r) - lower_bound(num.begin(), num.end(), l)));
-	} else {
-	    int ans =  (int)(upper_bound(num.begin(), num.end(), r) - lower_bound(num.begin(), num.end(), l));
-	    int ans1 =  ubb - lbb;
-	    if(ans != ans1)
-		printf("%lld %lld %d %d\n", l, r, ans, ans1);
-	}
+        ll l, r;
+        if(rrrr == 0)
+            scanf("%lld %lld", &l, &r);
+        else {
+            l = rand() % 10;
+            r = l + rand() % 10;
+            //printf("%lld %lld\n", l, r);
+        }
+
+        int lbb = lb(0, num.size(), l);
+        int ubb = ub(0, num.size(), r);
+        //printf("%d %d\n", ub(0, num.size(), r), (int)(upper_bound(num.begin(), num.end(), r) - num.begin()));
+
+        if(rrrr == 0) {
+            /*
+            printf("lb diff %d %d\n", lbb,  (int)(lower_bound(num.begin(), num.end(), l) -  num.begin()));
+            printf("ub diff %d %d\n", ubb, (int)(upper_bound(num.begin(), num.end(), r) -  num.begin()));
+            printf("%d\n", ubb - lbb);
+            */
+            printf("%d\n", (int)(upper_bound(num.begin(), num.end(), r) - lower_bound(num.begin(), num.end(), l)));
+        } else {
+            int ans =  (int)(upper_bound(num.begin(), num.end(), r) - lower_bound(num.begin(), num.end(), l));
+            int ans1 =  ubb - lbb;
+            if(ans != ans1)
+                printf("%lld %lld %d %d\n", l, r, ans, ans1);
+        }
     }
 
     return 0;
