a76f84b05abc2b25d1a4fe6f1d01f1ead8327882
Henry Tseng <henrybear327@gmail.com>
2016-05-27 20:39:30 +1000

    Finish Day1

--- HackerRank/30 Days of Code Challenges/src/Day1.java
+++ HackerRank/30 Days of Code Challenges/src/Day1.java
@@ -7,15 +7,16 @@ import java.util.regex.*;
 public class Day1 {
 	
     public static void main(String[] args) {
-        int i;
+        int i = 4;
         double d = 4.0;
         String s = "HackerRank ";
 		
         Scanner scan = new Scanner(System.in);
                 
-        i = scan.nextInt();
-        d = scan.nextDouble();
-        s += scan.nextLine();
+        i += scan.nextInt();
+        d += scan.nextDouble();
+        scan.nextLine();
+        s += scan.nextLine(); // just like fgets() after scanf()
         
         System.out.println(i);
         System.out.printf("%.1f\n", d);
