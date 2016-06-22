e45368c292dbc7b1db5514c6901fe9f32e4a6558
Henry Tseng <henrybear327@gmail.com>
2016-06-22 11:57:57 +1000

    Use random shuffle code

--- Codeforces/Div 2/Round 358/B/B.java
+++ Codeforces/Div 2/Round 358/B/B.java
@@ -3,12 +3,41 @@ import java.util.*;
 
 public class B
 {
+	public static void shuffleArray(int[] arr){
+        int n = arr.length;
+        Random rnd = new Random();
+        for(int i = 0; i < n; i++){
+            int tmp = arr[i];
+            int randomPos = i + rnd.nextInt(n-i);
+            arr[i] = arr[randomPos];
+            arr[randomPos] = tmp;
+        }   
+	}
+	
     public static void main(String[] args)
     {
         MyScanner sc = new MyScanner();
         out = new PrintWriter(new BufferedOutputStream(System.out));
 
         int n = sc.nextInt();
+        int[] inp = new int[n];
+        for(int i = 0; i < n; i++)
+        	inp[i] = sc.nextInt();
+        
+        shuffleArray(inp);;
+        Arrays.sort(inp);
+        
+        int ans = 1;
+        for(int i = 0; i < n; i++) {
+        	if(inp[i] >= ans)
+        		ans++;
+        	else if(inp[i] == ans - 1)
+        		continue;
+        	else
+        		break;
+        }
+        
+        /*
         ArrayList<Integer> inp = new ArrayList<Integer>();
         for(int i = 0; i < n; i++)
         	inp.add(sc.nextInt());
@@ -23,7 +52,9 @@ public class B
         		continue;
         	else
         		break;
-        }
+        } 
+         
+         */
         
         out.println(ans);
         
