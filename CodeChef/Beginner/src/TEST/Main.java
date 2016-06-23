0f83b70147f9ddd486e7c545479eaa6b78b6a34d
Henry Tseng <henrybear327@gmail.com>
2016-06-23 14:43:18 +1000

    Add CC TEST

---
+++ CodeChef/Beginner/src/TEST/Main.java
@@ -0,0 +1,82 @@
+package TEST;
+
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        while(sc.hasNext() == true) {
+        	int next = sc.nextInt();
+        	if(next == 42)
+        		break;
+        	out.println(next);
+        }
+        
+        out.close();
+    }
+
+    // PrintWriter for faster output
+    public static PrintWriter out;
+
+    // MyScanner class for faster input
+    public static class MyScanner
+    {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner()
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        boolean hasNext()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        String next()
+        {
+            if (hasNext())
+                return st.nextToken();
+            return null;
+        }
+
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
