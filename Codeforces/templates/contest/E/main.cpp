9cae832047fbb63312c1bd7b9907566f2db0c3d1
Henry Tseng <henrybear327@gmail.com>
2016-07-15 21:42:11 +0800

    Update CF template

--- Codeforces/templates/contest/E/main.cpp
+++ Codeforces/templates/contest/E/main.cpp
@@ -1,17 +1,13 @@
 #include <bits/stdc++.h>
-//LLONG_MIN LLONG_MAX INT_MIN INT_MAX
-
-#ifdef _WIN32
-#define lld "I64d"
-#else
-#define lld "lld"
-#endif
-
-typedef long long int ll;
 
 using namespace std;
 
+typedef long long int ll;
+typedef pair<int, int> ii;
+
 int main()
 {
+	
+	
     return 0;
 }
