f45b7a51ece6ffa4d2907364517d9d5f3b97f018
Henry Tseng <henrybear327@gmail.com>
2016-10-26 17:30:20 +0800

    add luckfour of CC

---
+++ CodeChef/Beginner/LUCKFOUR.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	while(n--) {
+		int num;
+		scanf("%d", &num);
+
+		int cnt = 0;
+		for(; num != 0; num /= 10)
+			if(num % 10 == 4)
+				cnt++;
+		printf("%d\n", cnt);
+	}
+
+	return 0;
+}
