b6bab42cbc12d2174c11aed5eda2fe969a42af9f
Henry Tseng <henrybear327@gmail.com>
2016-10-28 19:04:45 +0800

    Add 2 CC problems

---
+++ CodeChef/Beginner/SNAPE.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int ncase;
+	scanf("%d", &ncase);
+
+	while(ncase--) {
+		int b, ls;
+		scanf("%d %d", &b, &ls);
+
+		double r1 = sqrt(b * b + ls * ls), r2 = sqrt(ls * ls - b * b);
+		if(r1 - r2 > -1e-9)
+			swap(r1, r2);
+		printf("%.15f %.15f\n", r1, r2);
+	}
+
+	return 0;
+}
