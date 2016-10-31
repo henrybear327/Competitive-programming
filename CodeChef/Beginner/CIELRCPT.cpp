41ffcc97f5d77a3ace0881f10a423d986b25d988
Henry Tseng <henrybear327@gmail.com>
2016-10-31 11:58:08 +0800

    Add CC CIELRCPT

---
+++ CodeChef/Beginner/CIELRCPT.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int getCnt(int n)
+{
+	int cnt = 0;
+	while(n > 0) {
+		if((n & 1) == 1)
+			cnt++;
+		n >>= 1;
+	}
+	return cnt;
+}
+
+int main()
+{
+	int ncase;
+	scanf("%d", &ncase);
+
+	while(ncase--) {
+		int n;
+		scanf("%d", &n);
+
+		int quotient = n / 2048;
+		int remainder = n % 2048;
+
+		int cnt = quotient + getCnt(remainder);
+		printf("%d\n", cnt);
+	}
+
+	return 0;
+}
