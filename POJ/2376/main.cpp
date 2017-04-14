71ab8807b92049193bc89a9b01fda949e04d0ee4
Henry Tseng <henrybear327@gmail.com>
2017-04-14 09:57:17 +0800

    Finally got AC on 2376

---
+++ POJ/2376/main.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+typedef pair<int, int> ii;
+
+/*
+   1 5
+   2 5
+   3 6
+ */
+int main()
+{
+	int n, t;
+	while(scanf("%d %d", &n, &t) == 2) {
+		vector<ii> inp;
+		for(int i = 0; i < n; i++) {
+			int a, b;
+			scanf("%d %d", &a, &b);
+
+			inp.push_back(ii(a, b));
+		}
+		sort(inp.begin(), inp.end());
+
+		int begin = 1, end = 0, ans = 1;
+		for(int i = 0; i < n; i++) {
+			if(inp[i].first <= begin || inp[i].second <= end) { // the starting point is <= begin
+				if(inp[i].second > end) // extend the end point as far as you can
+					end = inp[i].second;
+			} else {
+				if(inp[i].first - end <= 1) // no gap between extended intervals
+					ans++;
+				else {
+					ans = -1;
+					break;
+				}
+				
+				begin = end + 1;
+				i--;
+			}
+		}
+
+		if(end < t) // crucial
+			printf("-1\n");
+		else
+			printf("%d\n", ans);
+	}
+
+	return 0;
+}
