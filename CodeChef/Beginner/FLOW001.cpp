066afe886e2c1f18202307127a0bb94d13142e0b
Henry Tseng <henrybear327@gmail.com>
2017-05-11 14:21:10 +0800

    Add several Codechef beginner problems

---
+++ CodeChef/Beginner/FLOW001.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+void solve()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+}
+
+int main()
+{
+	int ncase;
+	scanf("%d", &ncase);
+
+	while(ncase--) {
+		solve();
+	}
+
+	return 0;
+}
---
+++ CodeChef/Beginner/PERMUT2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+int n;
+bool solve()
+{
+	int inp[n + 1];
+	for(int i = 1; i <= n; i++)
+		scanf("%d", &inp[i]);
+
+	int cnt = 0;
+	int nxt[n + 1];
+	for(int i = 1; i <= n; i++) 
+		nxt[inp[i]] = i;
+	
+	for(int i = 1; i <= n; i++)
+		if(inp[i] == nxt[i])
+			cnt++;
+	
+	return cnt == n;
+}
+
+int main()
+{
+	while(scanf("%d", &n) && n != 0) {
+		printf("%s\n", solve() ? "ambiguous" : "not ambiguous");
+	}
+
+	return 0;
+}
