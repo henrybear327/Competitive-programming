985525d1fb24e0763124f25058f7a252d7ea7abd
Henry Tseng <henrybear327@gmail.com>
2017-05-14 23:13:15 +0800

    Add 2 Codechef problems

---
+++ CodeChef/Beginner/FLOW006.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+void solve()
+{
+	int n;
+	scanf("%d", &n);
+
+	int res = 0;
+	while(n) {
+		res += n % 10;
+		n /= 10;
+	}
+	printf("%d\n", res);
+}
+
+int main()
+{
+	int ncase;
+	scanf("%d", &ncase);
+
+	while(ncase--) {
+		solve();
+	}
+
+	return 0;
+}
