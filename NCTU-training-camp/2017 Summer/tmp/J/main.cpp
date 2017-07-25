01a36cfc6451aa9f0652752a412fde54bd6dfafd
Henry Tseng <henrybear327@gmail.com>
2017-07-25 00:29:29 +0000

    Add 2 prob

---
+++ NCTU-training-camp/2017 Summer/tmp/J/main.cpp
@@ -0,0 +1,13 @@
+// http://codeforces.com/problemset/problem/579/A
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", __builtin_popcount(n));
+    
+    return 0;
+}
---
+++ NCTU-training-camp/2017 Summer/tmp/K/main.cpp
@@ -0,0 +1,13 @@
+// http://codeforces.com/problemset/problem/579/A
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m, a;
+    scanf("%d %d %d", &n, &m, &a);
+    printf("%d\n", ...);
+    
+    return 0;
+}
