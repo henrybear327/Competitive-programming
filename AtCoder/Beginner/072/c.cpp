77afe518cfc4418e5bb553a69f478299d098cb73
Henry Tseng <henrybear327@gmail.com>
2017-09-06 22:17:15 +0800

    Finish Beginner 072

--- AtCoder/Beginner/072/c.cpp
+++ AtCoder/Beginner/072/c.cpp
@@ -8,16 +8,16 @@ int main()
     scanf("%d", &n);
 
     int cnt[100010] = {0};
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         int num;
         scanf("%d", &num);
 
         cnt[num]++;
     }
-    
+
     int mx = cnt[0] + cnt[1] + cnt[2];
     int sum = mx;
-    for(int i = 1; i < 100010 - 2; i++) {
+    for (int i = 1; i < 100010 - 2; i++) {
         sum += cnt[i + 2];
         sum -= cnt[i - 1];
 
--- AtCoder/Beginner/072/d.cpp
+++ AtCoder/Beginner/072/d.cpp
@@ -7,34 +7,20 @@ int main()
     int n;
     scanf("%d", &n);
 
-    int inp[n + 1], inp1[n + 1];
-    for(int i = 1; i <= n; i++) {
+    int inp[n];
+    for (int i = 0; i < n; i++) {
         scanf("%d", &inp[i]);
-        inp1[i] = inp[i];
     }
 
-    int mn = INT_MAX;
-    {
-        int cnt = 0;
-        for(int i = 1; i <= n - 1; i++) {
-            if(inp[i] == i) {
-                swap(inp[i], inp[i + 1]);
-                cnt++;
-            }
+    int cnt = 0;
+    for (int i = 0; i < n - 1; i++)
+        if (inp[i] == i + 1) {
+            swap(inp[i], inp[i + 1]);
+            cnt++;
         }
-        mn = min(mn, cnt);
-    }
+    if (inp[n - 1] == n)
+        cnt++;
 
-    {
-        int cnt = 0;
-        for(int i = n; i >= 2; i--) {
-            if(inp1[i] == i) {
-                swap(inp1[i], inp1[i - 1]);
-                cnt++;
-            }
-        }
-        mn = min(mn, cnt);
-    }
-    printf("%d\n", mn);
+    printf("%d\n", cnt);
     return 0;
 }
