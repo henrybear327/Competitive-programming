09ad30ac3323ab0e634df09c6e20bf2a5bf7a154
Henry Tseng <henrybear327@gmail.com>
2018-01-11 10:04:29 +0800

    Add Hello 2018 B

--- Codeforces/Div 2/Hello2018/b.cpp
+++ Codeforces/Div 2/Hello2018/b.cpp
@@ -7,7 +7,34 @@ typedef pair<int, int> ii;
 
 int main()
 {
+	int n;
+	scanf("%d", &n);
 	
+	int par[n];
+	par[0] = -1;
+	int in[n];
+	memset(in, 0, sizeof(in));
+	for(int i = 1; i < n; i++) {
+		scanf("%d", &par[i]);
+		par[i]--;
+		in[par[i]]++;
+	}
+
+	int dp[n];
+	memset(dp, 0, sizeof(dp));
+	for(int i = n - 1; i >= 1; i--) {
+		if(in[i] == 0) 
+			dp[par[i]]++;
+	}
+	
+	bool ok = true;
+	for(int i = 0; i < n; i++) {
+		if(in[i] > 0) {
+			if(dp[i] < 3)
+				ok = false;
+		}
+	}
+	printf("%s\n", ok ? "Yes" : "No");
 	
     return 0;
 }
