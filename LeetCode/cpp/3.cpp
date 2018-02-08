88e78ef61ee1434c336ac63d24beb6de7e9b4712
Henry Tseng <henrybear327@gmail.com>
2018-02-08 22:15:53 +0800

    Add 3

---
+++ LeetCode/cpp/3.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        // two pointers
+
+        int l = 0;
+        map<char, int> cnt;
+        int ans = 0;
+        for (int r = 0; r < (int)s.length(); r++) {
+            cnt[s[r]]++;
+
+            if (cnt[s[r]] > 1) {
+                while (l <= r && cnt[s[r]] > 1) {
+                    cnt[s[l]]--;
+                    l++;
+                }
+            }
+
+            ans = max(ans, r - l + 1);
+        }
+
+        // no need to check afterwards since the answer will only be shorter
+
+        return ans;
+    }
+};
+
+int main()
+{
+    string s = "abcabcbb";
+    Solution ss;
+    printf("%d\n", ss.lengthOfLongestSubstring(s));
+    return 0;
+}
