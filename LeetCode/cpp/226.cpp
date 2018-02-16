a9c45c37f2a8ad0b80eac02a53cefe557de12080
Henry Tseng <henrybear327@gmail.com>
2018-02-16 18:54:14 +0800

    Add 1 leetcode

---
+++ LeetCode/cpp/226.cpp
@@ -0,0 +1,46 @@
+// clang-format -style=LLVM -i *.cpp && astyle --style=linux *.cpp && rm *.orig
+// && g++ -Wall -Wextra -std=c++11 ...
+#include <bits/stdc++.h>
+using namespace std;
+
+static int initialSetup = []()
+{
+    // toggle off cout & cin, instead, use printf & scanf
+    std::ios::sync_with_stdio(false);
+    // untie cin & cout
+    cin.tie(NULL);
+    return 0;
+}
+();
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// simply do reverse at all levels
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == NULL)
+            return NULL;
+
+        invertTree(root->right);
+        invertTree(root->left);
+
+        auto tmp = root->right;
+        root->right = root->left;
+        root->left = tmp;
+
+        return root;
+    }
+};
+
+int main()
+{
+    return 0;
+}
