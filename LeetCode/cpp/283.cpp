60d8da1b7c8e44cce50c9577fd25521993a12d01
Henry Tseng <henrybear327@gmail.com>
2018-03-03 10:03:00 +0800

    add 1 leetcode

---
+++ LeetCode/cpp/283.cpp
@@ -0,0 +1,107 @@
+// clang-format -style=LLVM -i *.cpp && astyle --style=linux *.cpp && rm *.orig
+// && g++ -Wall -Wextra -std=c++11 ...
+#include <bits/stdc++.h>
+using namespace std;
+
+static int initialSetup = []()
+{
+    // toggle off cout & cin, instead, use printf & scanf
+    std::ios::sync_with_stdio(false);
+    // untie cin & cout
+    cin.tie(NULL);
+    return 0;
+}
+();
+
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int zero = 0;
+        int nonzero = 0;
+        int n = (int)nums.size();
+        while (zero < n && nonzero < n) {
+            while (zero < n && nums[zero] != 0) {
+                zero++;
+            }
+
+            while (nonzero < n && nonzero < zero) { // WA
+                nonzero++;
+            }
+
+            while (nonzero < n && nums[nonzero] == 0) {
+                nonzero++;
+            }
+
+            if (zero < n && nonzero < n) // WA
+                swap(nums[nonzero], nums[zero]);
+        }
+    }
+};
+
+void trimLeftTrailingSpaces(string &input)
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(),
+    [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input)
+{
+    input.erase(
+    find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    })
+    .base(),
+    input.end());
+}
+
+vector<int> stringToIntegerVector(string input)
+{
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1)
+{
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for (int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+
+        Solution().moveZeroes(nums);
+
+        string out = integerVectorToString(nums);
+        cout << out << endl;
+    }
+    return 0;
+}
