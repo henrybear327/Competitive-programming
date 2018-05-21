3fd9c2dc556361fa06a9311678232ce70c3f1ded
Henry Tseng <henrybear327@gmail.com>
2018-05-21 11:21:26 +0800

    add R1C C

---
+++ Google/CodeJam/2018/R1C/c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll dp[100010][555];
+const ll oo = 0x3f3f3f3f3f3f3f3f;
+
+int solve()
+{
+    int n;
+    scanf("%d", &n);
+
+    ll w[n + 1];
+    for (int i = 1; i <= n; i++)
+        scanf("%lld", &w[i]);
+
+    for (int i = 0; i <= n; i++)
+        fill(dp[i], dp[i] + 555, oo);
+    dp[0][0] = 0;
+    for (int i = 1; i <= n; i++) {
+        dp[i][0] = 0;
+        for (int j = 1; j < 555; j++) {
+            if (dp[i - 1][j - 1] <= 6LL * w[i]) {
+                dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + w[i]);
+            }
+
+            dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+        }
+    }
+
+    for (int i = 555 - 1; i >= 0; i--)
+        if (dp[n][i] != oo)
+            return i;
+    return 0;
+}
+
+int main()
+{
+    int ncase;
+    scanf("%d", &ncase);
+    for (int i = 1; i <= ncase; i++) {
+        printf("Case #%d: %d\n", i, solve());
+    }
+}
