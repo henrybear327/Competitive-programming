499f5a42b33fb0ffd42647e70547746b27949279
Henry Tseng <henrybear327@gmail.com>
2018-07-03 22:38:30 +0800

    add 6 LC

---
+++ LeetCode/cpp/746.cpp
@@ -0,0 +1,37 @@
+// :%s/^ \* //g
+#ifdef LOCAL
+#include <bits/stdc++.h>
+using namespace std;
+
+// define data structures here
+#endif
+
+static int __initialSetup = []()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
+();
+
+class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        int n = cost.size();
+        vector<int> dp(n + 1, 0);
+        for (int i = 2; i <= n; i++) {
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[n];
+    }
+};
+
+#ifdef LOCAL
+int main()
+{
+    return 0;
+}
+#endif
