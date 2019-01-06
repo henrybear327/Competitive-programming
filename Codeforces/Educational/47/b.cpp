4e1b5201f86a53613d27ed91b9343db33263844b
Henry Tseng <henrybear327@gmail.com>
2019-01-06 12:20:41 +0800

    add CF edu 47 BC

--- Codeforces/Educational/47/b.cpp
+++ Codeforces/Educational/47/b.cpp
@@ -7,7 +7,43 @@ typedef pair<int, int> ii;
 
 int main()
 {
+    char inp[100010];
+    scanf("%s", inp);
+    
+    // find first 2
+    int sz = strlen(inp);
+    int firstTwo = -1;
+    for(int i = 0; i < sz; i++) {
+        if(inp[i] == '2') {
+            firstTwo = i;
+            sort(inp, inp + i);
+            break;
+        }
+    }   
 
+    if(firstTwo == -1) {
+        sort(inp, inp + sz);
+        printf("%s", inp);
+    } else {
+        // move all 1s to front
+        int one = 0;
+        for(int i = firstTwo; i < sz; i++)
+            if(inp[i] == '1')
+                one++;
+        
+        char ans[100010];
+        strcpy(ans, inp);
+
+        for(int i = 0; i < one; i++)
+            ans[i + firstTwo] = '1';
+
+        int idx = firstTwo + one;
+        for(int i = firstTwo; i < sz; i++) {
+            if(inp[i] != '1')
+                ans[idx++] = inp[i];
+        }
+        printf("%s", ans);
+    }
 
     return 0;
 }
--- Codeforces/Educational/47/c.cpp
+++ Codeforces/Educational/47/c.cpp
@@ -7,7 +7,42 @@ typedef pair<int, int> ii;
 
 int main()
 {
+    int n, m;
+    scanf("%d %d", &n, &m);
 
+    long double sol = 0.0;
+    
+    ll oddSum = 0;
+    ll evenSum = 0;
+    ll sum = 0;
+    for(int i = 0; i < m; i++) {
+        ll x, k;
+        scanf("%d %d", &x, &k);
+
+        sum += x;
+        if(k >= 0) {
+            oddSum += k * (n - 1) * n;
+            // printf("k >= 0 %f\n", sol);
+        } else {
+            bool even = n % 2 == 0;
+            if(even)
+                n--;
+            int cnt = n / 2;
+            ll inc = (k + cnt * k) * cnt;
+            evenSum += inc;
+            if(even)
+                n++;
+            cnt = n / 2;
+            evenSum += (cnt * k);
+            // printf("k < 0 %f\n", sol);
+        }
+    }
+
+    sol += (1.0 * oddSum / 2) + (1.0 * evenSum);
+    sol /= n;
+    sol += sum;
+
+    printf("%.15Lf\n", sol);
 
     return 0;
 }
