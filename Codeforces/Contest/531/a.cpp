a7211d77d21ff916e7aceaa266156eb39760e32e
Henry Tseng <henrybear327@gmail.com>
2019-02-16 18:27:09 +0800

    add 531 A

--- Codeforces/Contest/531/a.cpp
+++ Codeforces/Contest/531/a.cpp
@@ -7,14 +7,14 @@ typedef pair<int, int> ii;
 
 int main()
 {
-	int n;
-	scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-	n %= 4;
-	if (n == 1 || n == 2)
-		printf("1\n");
-	else
-		printf("0\n");
+    n %= 4;
+    if (n == 1 || n == 2)
+        printf("1\n");
+    else
+        printf("0\n");
 
-	return 0;
+    return 0;
 }
