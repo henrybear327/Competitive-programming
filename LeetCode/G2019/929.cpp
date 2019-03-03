1ef3a5c39f8790cde05382a7a34ef0c247e74bb6
Henry Tseng <henrybear327@gmail.com>
2019-03-04 00:43:35 +0800

    add 1 LC

---
+++ LeetCode/G2019/929.cpp
@@ -0,0 +1,30 @@
+static int desyncio = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_set<string> s;
+        for(auto i : emails) {
+            int at = i.find("@");
+            string username = i.substr(0, at);
+            string domain = i.substr(at + 1);
+            
+            int plus = username.find("+");
+            if(plus != string::npos) {
+                username = username.substr(0, plus);
+            }
+            
+            string newUsername;
+            for(auto j : username)
+                if(j != '.')
+                    newUsername += j;
+            s.insert(newUsername + "@" + domain);
+        }
+        return s.size();
+    }
+};
