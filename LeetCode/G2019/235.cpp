0b51c70b8fd050d4181c825555f83e4b8bd1e44d
Henry Tseng <henry@protonmail.com>
2019-03-08 16:39:53 +0800

    format code

--- LeetCode/G2019/235.cpp
+++ LeetCode/G2019/235.cpp
@@ -7,21 +7,23 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
         // use BST property
-        if(root == NULL)
+        if (root == NULL)
             return NULL;
-        
+
         int vp = p->val;
         int vq = q->val;
-        if(vp > vq)
+        if (vp > vq)
             swap(vp, vq);
-        
-        if(vp <= root->val && root->val <= vq) // the first splitting node
+
+        if (vp <= root->val && root->val <= vq) // the first splitting node
             return root;
-        else if(vp < root->val && vq < root->val)
+        else if (vp < root->val && vq < root->val)
             return lowestCommonAncestor(root->left, p, q);
         return lowestCommonAncestor(root->right, p, q);
     }
@@ -42,13 +44,13 @@ public:
 //     bool lca(TreeNode* root, TreeNode* p, TreeNode *q) {
 //         if(root == NULL)
 //             return false;
-        
+
 //         bool found = false;
 //         if(root == p || root == q)
 //             found = true;
 //         bool l = lca(root->left, p, q);
 //         bool r = lca(root->right, p, q);
-        
+
 //         if(found && (l || r)) { // self + one child -> self
 //             ans = root;
 //             return true;
@@ -57,14 +59,15 @@ public:
 //             ans = root;
 //             return true;
 //         }
-        
+
 //         return found || r || l;
 //     }
 // public:
-//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+//     {
 //         // only one query per test case
 //         // why bother to use the LCA data structure??
-        
+
 //         ans = NULL;
 //         lca(root, p, q);
 //         return ans;
--- LeetCode/G2019/27.cpp
+++ LeetCode/G2019/27.cpp
@@ -1,14 +1,16 @@
-class Solution {
+class Solution
+{
 public:
-    int removeElement(vector<int>& nums, int val) {
+    int removeElement(vector<int> &nums, int val)
+    {
         int put = 0;
-        for(int i = 0; i < (int)nums.size(); i++) {
-            if(nums[i] != val) {
+        for (int i = 0; i < (int)nums.size(); i++) {
+            if (nums[i] != val) {
                 swap(nums[i], nums[put]);
                 put++;
             }
         }
-        
+
         return put;
     }
 };
