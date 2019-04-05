a921ee14290a445ac96cbd056bb3180543cc0ac6
Henry Tseng <henry@protonmail.com>
2019-04-05 21:56:16 +0800

    format 547 ABCD

--- Codeforces/Contest/547/b.cpp
+++ Codeforces/Contest/547/b.cpp
@@ -7,24 +7,24 @@ typedef pair<int, int> ii;
 
 int main()
 {
-	int n;
-	scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-	int inp[n * 2];
-	for(int i = 0; i < n; i++)
-		scanf("%d", &inp[i]), inp[i + n] = inp[i];
-	
-	int ans = 0;
-	for(int i = 0; i < n; i++) {
-		if(inp[i] == 1) {
-			int j = i;
-			while(j < 2 * n && inp[j] == 1)
-				j++;
-			ans = max(j - i, ans);
-			i = j - 1;
-		}
-	}
-	printf("%d\n", ans);
+    int inp[n * 2];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inp[i]), inp[i + n] = inp[i];
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (inp[i] == 1) {
+            int j = i;
+            while (j < 2 * n && inp[j] == 1)
+                j++;
+            ans = max(j - i, ans);
+            i = j - 1;
+        }
+    }
+    printf("%d\n", ans);
 
     return 0;
 }
