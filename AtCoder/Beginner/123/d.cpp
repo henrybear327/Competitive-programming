3708e3ded08ce79ee6e716c76c7617932033cc4f
Henry Tseng <henry@protonmail.com>
2019-04-14 22:49:13 +0800

    add Atcoder Beginner 123 D

--- AtCoder/Beginner/123/d.cpp
+++ AtCoder/Beginner/123/d.cpp
@@ -2,43 +2,75 @@
 using namespace std;
 
 typedef long long ll;
-typedef pair<int, int> ii;
+typedef pair<int, pair<int, int>> iii;
+
+// BFS for solution!
+// for every move, you can generate 3 more candidates
+// you can push the value to a pq!
+
+const int N = 3333;
+ll inpX[N], inpY[N], inpZ[N];
+struct Node {
+    int x, y, z;
+    bool operator<(const Node &others) const
+    {
+        ll s1 = inpX[x] + inpY[y] + inpZ[z];
+        ll s2 = inpX[others.x] + inpY[others.y] + inpZ[others.z];
+
+        return s1 < s2;
+    }
+
+    ll val()
+    {
+        return inpX[x] + inpY[y] + inpZ[z];
+    }
+};
 
 int main()
 {
     int x, y, z, k;
     scanf("%d %d %d %d", &x, &y, &z, &k);
 
-    ll inpX[x], inpY[y], inpZ[z];
     for (int i = 0; i < x; i++)
         scanf("%lld", &inpX[i]);
+    sort(inpX, inpX + x);
     for (int i = 0; i < y; i++)
         scanf("%lld", &inpY[i]);
+    sort(inpY, inpY + y);
     for (int i = 0; i < z; i++)
         scanf("%lld", &inpZ[i]);
     sort(inpZ, inpZ + z);
-	
-	// we generate all results between first 2 arrays O(x*y)
-    ll cand[x * y];
-    for (int i = 0; i < x; i++)
-        for (int j = 0; j < y; j++)
-            cand[i * x + j] = inpX[i] + inpY[j];
-    sort(cand, cand + x * y); // O(x*y log(x*y))
 
-    vector<ll> ans;
-	// we take the first k elements, and generate all results, too
-	// O(k * k)
-    for (int xx = max(0, x * y - k); xx < x * y; xx++)
-        for (int yy = max(0, z - k); yy < z; yy++) {
-            // printf("xx %d yy %d\n", xx, yy);
-            // printf("%lld %lld\n", cand[xx], inpZ[yy]);
-            ans.push_back(cand[xx] + inpZ[yy]);
+    priority_queue<Node> pq;
+    set<iii> used;
+    used.insert({x - 1, {y - 1, z - 1}});
+    pq.push({x - 1, y - 1, z - 1});
+    while (pq.size() > 0 && k--) {
+        Node cur = pq.top();
+        pq.pop();
+        printf("%lld\n", cur.val());
+
+        if (cur.x > 0) {
+            auto newNode = cur;
+            newNode.x--;
+            iii c = {newNode.x, {newNode.y, newNode.z}};
+            if (used.find(c) == used.end())
+                pq.push(newNode), used.insert(c);
         }
-    sort(ans.begin(), ans.end());
-    reverse(ans.begin(), ans.end());
-
-    for (int xx = 0; xx < k; xx++) {
-        printf("%lld\n", ans[xx]);
+        if (cur.y > 0) {
+            auto newNode = cur;
+            newNode.y--;
+            iii c = {newNode.x, {newNode.y, newNode.z}};
+            if (used.find(c) == used.end())
+                pq.push(newNode), used.insert(c);
+        }
+        if (cur.z > 0) {
+            auto newNode = cur;
+            newNode.z--;
+            iii c = {newNode.x, {newNode.y, newNode.z}};
+            if (used.find(c) == used.end())
+                pq.push(newNode), used.insert(c);
+        }
     }
 
     return 0;
