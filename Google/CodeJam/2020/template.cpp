f30ce6b3c50bd74095f92c28f5401035eba436d2
Henry Tseng <henrybear327@gmail.com>
2020-04-05 18:42:40 +0800

    add google codejam template

---
+++ Google/CodeJam/2020/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {}
+
+int main()
+{
+    int ncase;
+    scanf("%d", &ncase);
+
+    for (int i = 1; i <= ncase; i++) {
+        printf("Case #%d: ", i);
+        solve();
+        printf("\n");
+    }
+
+    return 0;
+}
