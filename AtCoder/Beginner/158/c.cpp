364475006fafa96043a30b46493a72846ab139ef
Henry Tseng <henrybear327@gmail.com>
2020-04-11 00:16:31 +0800

    add math sol of C

--- AtCoder/Beginner/158/c.cpp
+++ AtCoder/Beginner/158/c.cpp
@@ -8,12 +8,23 @@ int main()
 {
     int a, b;
     scanf("%d %d", &a, &b);
+    /*
     for (double i = 1; i <= 1251; i++)
         if ((int)(i * 0.08) == a && b == (int)(i * 0.1)) {
             printf("%d\n", (int)i);
             return 0;
         }
     printf("-1\n");
+    */
+
+    int left = (int)ceil(max(1.0 * a / 0.08, 1.0 * b / 0.1));
+    int right = (int)floor(min(1.0 * (a + 1) / 0.08, 1.0 * (b + 1) / 0.1));
+
+    // printf("%d %d\n", left, right);
+    if (left < right)
+        printf("%d\n", left);
+    else
+        printf("-1\n");
 
     return 0;
 }
