e381dc7fc6276d0c65c7817a33b70f3d3a3d44da
Henry Tseng <henrybear327@gmail.com>
2020-04-14 13:51:18 +0800

    add 1 LC

--- LeetCode/2020_30-day-chal/contigiousArray.cpp
+++ LeetCode/2020_30-day-chal/contigiousArray.cpp
@@ -22,3 +22,29 @@ public:
         return ans;
     }
 };
+
+class Solution {
+// binary search won't work!!
+// 11000011
+
+public:
+    int findMaxLength(vector<int>& nums) {
+        int sum = 0;
+        int prev[nums.size() * 2 + 1];
+        fill(prev, prev + nums.size() * 2 + 1, -1);
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(prev[sum + nums.size()] == -1)
+                prev[sum + nums.size()] = i;
+            else
+                ans = max(ans, i - prev[sum + nums.size()]);
+
+            sum += nums[i] == 1 ? 1 : -1;
+        }
+
+        if(prev[sum + nums.size()] != -1)
+            ans = max(ans, (int)nums.size() - prev[sum + nums.size()]);
+
+        return ans;
+    }
+};
