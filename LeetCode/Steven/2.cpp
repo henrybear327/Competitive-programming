8457a90c40b9396b74502becaf762b68937c0b66
Henry Tseng <henrybear327@gmail.com>
2020-12-08 14:50:12 +0800

    add 1 LC and reformat

--- LeetCode/Steven/2.cpp
+++ LeetCode/Steven/2.cpp
@@ -8,33 +8,35 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* head = NULL, *cur = NULL;
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *head = NULL, *cur = NULL;
         int carry = 0;
-        while(l1 != NULL || l2 != NULL || carry > 0) {
+        while (l1 != NULL || l2 != NULL || carry > 0) {
             int num1 = l1 != NULL ? l1->val : 0;
             int num2 = l2 != NULL ? l2->val : 0;
-            
+
             int num = num1 + num2 + carry;
             carry = num >= 10;
             num %= 10;
-            
-            if(head == NULL) {
+
+            if (head == NULL) {
                 head = cur = new ListNode(num);
             } else {
                 auto tmp = new ListNode(num);
                 cur->next = tmp;
                 cur = tmp;
             }
-            
-            if(l1 != NULL)
+
+            if (l1 != NULL)
                 l1 = l1->next;
-            if(l2 != NULL)
+            if (l2 != NULL)
                 l2 = l2->next;
         }
-        
+
         return head;
     }
 };
--- LeetCode/Steven/344.cpp
+++ LeetCode/Steven/344.cpp
@@ -1,11 +1,11 @@
-class Solution {
+class Solution
+{
 public:
-    void reverseString(vector<char>& s) {
+    void reverseString(vector<char> &s)
+    {
         // reverse(s.begin(), s.end());
-        for(int i = 0; i < s.size() / 2; i++) {
+        for (int i = 0; i < s.size() / 2; i++) {
             swap(s[i], s[s.size() - 1 - i]);
         }
     }
 };
-
-
