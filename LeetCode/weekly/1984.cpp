ada2073f88c8b01d45b3fc5796def5355c4b0eec
Henry Tseng <henrybear327@gmail.com>
2021-09-04 19:52:58 +0200

    add 2 LC

---
+++ LeetCode/weekly/1984.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        
+        int l = 0, r = k - 1;
+        int ans = nums[r] - nums[l];
+        
+        for(; r < nums.size(); l++, r++) {
+            ans = min(ans, nums[r] - nums[l]);
+        }
+        
+        return ans;
+    }
+};
